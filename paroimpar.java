42b8cbe0f3adfa5cf63c329626f78599700d3be4
sebastianchacon <chaconcarrillosebastian@gmail.com>
2021-02-02 13:09:50 -0300

    documentacion

--- paroimpar.java
+++ paroimpar.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
 // hacemos publico el programa
+
 public class NumeroParImpar
 {
     // aqui creamos la funcion
     public static void main( String[] args )
     {
+        // Hacemos que el numero que sea ingresado sea entero
         int numero;
         Scanner teclado = new Scanner( System.in );
 
+        // Introducimos el numero por consola
         System.out.printf( "Introduzca un número entero: " );
         numero = teclado.nextInt();
 
+        // aca hacemos la logica del programa
         if ( numero % 2 == 0 )
         {
             System.out.printf( "ES PAR" );
